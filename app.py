45a8bdd47faa4ccbb790f2edf95ae1e1565b1982
99Pezzato <pezzato99@icloud.com>
2025-11-09 11:19:42 +0000

    Fix NaN issue and align frontend JS with backend API

--- app.py
+++ app.py
@@ -46,5 +46,6 @@ def api_state():
 
 # --- Run app (Render) ---
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
